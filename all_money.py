66cee20779a5cf626faaa58d65228caa6592d081
tantan31418 <tantan31418@gmail.com>
2019-08-19 02:15:46 +0800

    firstfirst

---
+++ all_money.py
@@ -0,0 +1,62 @@
+money=[
+
+{'ID': '1', 'money_2_pay': 229, 'paid': False},
+
+{'ID': '2', 'money_2_pay': 165, 'paid': False},
+
+{'ID': '3', 'money_2_pay': 185, 'paid': False},
+
+{'ID': '4', 'money_2_pay': 229, 'paid': False},
+
+{'ID': '5', 'money_2_pay': 272, 'paid': False},
+
+{'ID': '6', 'money_2_pay': 196, 'paid': False},
+
+{'ID': '7', 'money_2_pay': 218, 'paid': False},
+
+{'ID': '8', 'money_2_pay': 196, 'paid': False},
+
+{'ID': '9', 'money_2_pay': 234, 'paid': False},
+
+{'ID': '10', 'money_2_pay': 185, 'paid': False},
+
+{'ID': '11', 'money_2_pay': 377, 'paid': False},
+
+{'ID': '12', 'money_2_pay': 241, 'paid': False},
+
+{'ID': '13', 'money_2_pay': 165, 'paid': False},
+
+{'ID': '14', 'money_2_pay': 0, 'paid': False},
+
+{'ID': '15', 'money_2_pay': 196, 'paid': False},
+
+{'ID': '16', 'money_2_pay': 338, 'paid': False},
+
+{'ID': '17', 'money_2_pay': 185, 'paid': False},
+
+{'ID': '18', 'money_2_pay': 239, 'paid': False},
+
+{'ID': '19', 'money_2_pay': 196, 'paid': False},
+
+{'ID': '20', 'money_2_pay': 0, 'paid': False},
+
+{'ID': '21', 'money_2_pay': 207, 'paid': False},
+
+{'ID': '22', 'money_2_pay': 273, 'paid': False},
+
+{'ID': '23', 'money_2_pay': 316, 'paid': False},
+
+{'ID': '24', 'money_2_pay': 0, 'paid': False},
+
+{'ID': '25', 'money_2_pay': 0, 'paid': False},
+
+{'ID': '26', 'money_2_pay': 272, 'paid': False},
+
+{'ID': '27', 'money_2_pay': 373, 'paid': False},
+
+{'ID': '28', 'money_2_pay': 346, 'paid': False},
+
+{'ID': '29', 'money_2_pay': 0, 'paid': False},
+
+{'ID': '30', 'money_2_pay': 229, 'paid': False}
+]
---
+++ main.py
@@ -0,0 +1,65 @@
+from left_money import *
+from all_money import *
+
+Moname =[1,5,10,50,100,500,1000]
+
+user = int(input('your #'))
+your_money = int(input('your $'))
+
+total_ammount=0
+
+need_to_return=your_money-money[user-1]['money_2_pay']
+
+#print(return_money(need_to_return))
+
+# compute how to give change
+def return_money(nr):
+    return_change = [0 for x in range(6)]
+    if nr > 500:
+        while left_money['500'] !=0:
+            return_change[5] +=1
+            nr-=500
+    if nr>100:
+        while left_money['100'] !=0:
+            return_change[4] +=1
+            nr-=100
+    if nr>50:
+        while left_money['50'] !=0:
+            return_change[3] +=1
+            nr-=50
+    if nr>10:
+        while left_money['10'] !=0:
+            return_change[2] +=1
+            nr-=10
+    if nr>5:
+        while left_money['5'] !=0:
+            return_change[1] +=1
+            nr-=5
+    if nr>1:
+        while left_money['1'] !=0:
+            return_change[0] +=1
+            nr-=1
+    if nr:
+        print('無法找錢，請稍候再來')
+    else :
+        for x in range(6):
+            print('{}個{}元'.format(return_change[x],Moname[x]))
+return_money(need_to_return)
+'''
+# manually change change
+def change_change(aadd):
+    pass
+    #left_money[change_n] +=aadd
+這個部分先暫停
+
+
+# print status
+# 其實另外開一個檔案監控就可以了（？
+# 或許可以像斂晶場記分板那樣（？
+
+
+def print_remain():
+    print('目前收到{}元'.format(total_ammount))
+    #print('還有{}人沒交錢，分別是{}'.format())
+    print(left_money)
+'''
